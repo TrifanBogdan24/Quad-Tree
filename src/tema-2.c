7468822823fe91ad3c33fbd52f133c77a527957a
TrifanBogdan24 <trifanbogdan24@gmail.com>
2025-08-15 15:56:05 +0300

    Trying task1

--- src/tema-2.c
+++ src/tema-2.c
@@ -2,11 +2,128 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include "utils.h"
+
+/*
+* (x, y) -> coordinates of upper-left corner
+*/
+unsigned int compute_mean(RawImage *img, unsigned int x, unsigned int y, unsigned int size)
+{
+    if (!size) {
+        fprintf(stderr, "[ERR] Cannot divide with size=0\n");
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned long long sum_R = 0;
+    unsigned long long sum_G = 0;
+    unsigned long long sum_B = 0;
+
+    for (int i = x; i < x + size; i++) {
+        for (int j = y; j < y + size; j++) {
+            sum_R += img->grid[i][j].R;
+            sum_G += img->grid[i][j].G;
+            sum_B += img->grid[i][j].B;
+        }
+    }
+
+    unsigned int mean_R = sum_R / (size * size);
+    unsigned int mean_G = sum_G / (size * size);
+    unsigned int mean_B = sum_B / (size * size);
+
+    unsigned long long sum_square_diff = 0;
+
+
+    for (int i = x; i < x + size; i++) {
+        for (int j = y; j < y + size; j++) {
+            sum_square_diff += (mean_R - img->grid[i][j].R) * (mean_R - img->grid[i][j].R);
+            sum_square_diff += (mean_G - img->grid[i][j].G) * (mean_G - img->grid[i][j].G);
+            sum_square_diff += (mean_B - img->grid[i][j].B) * (mean_B - img->grid[i][j].B);
+        }
+    }
+
+    return (sum_square_diff / (3 * size * size));
+}
+
+RawImage read_PPM_image(FILE *fin)
+{
+    char type[2];
+    fread(type, sizeof(char), sizeof(type), fin);
+
+    RawImage img;
+    fscanf(fin, "%u", &img.width);
+    fscanf(fin, "%u", &img.height);
+    fscanf(fin, "%u", &img.max_rgb);
+
+    img.grid = (color **) malloc(img.height * sizeof(color *));
+    for (int i = 0; i < img.height; i++)
+        img.grid[i] = (color *) malloc(img.width * sizeof(color));
+    
+    for (int i = 0; i < img.height; i++) {
+        for (int j = 0; j < img.width; j++) {
+            fread(&img.grid[i][j].R, sizeof(unsigned char), 1, fin);
+            fread(&img.grid[i][j].G, sizeof(unsigned char), 1, fin);
+            fread(&img.grid[i][j].B, sizeof(unsigned char), 1, fin);
+        }
+    }
+
+    return img;
+}
 
 
 void solve_task1(int factor, FILE *fin, FILE *fout)
 {
-    // TODO
+    RawImage img = read_PPM_image(fin);
+    return;
+    QuadTree *root = new_tree_node(0, 0, img.height);
+
+    Queue *queue = new_empty_queue();
+    queue_push(&queue, root);
+
+    while (!is_empty(queue)) {
+        QuadTree *node = queue_pop(queue);
+        unsigned int x = node->x;
+        unsigned int y = node->y;
+        unsigned int size = node->size;
+        unsigned int mean = compute_mean(&img, x, y, size);
+        printf("%d %d\n", size, mean);
+
+        if (mean <= factor)
+            continue;
+        
+        node->child_upper_left  = new_tree_node(x         , y         , size/2);
+        node->child_upper_right = new_tree_node(x + size/2, y         , size/2);
+        node->child_lower_right = new_tree_node(x + size/2, y + size/2, size/2);
+        node->child_lower_left  = new_tree_node(x         , y + size/2, size/2); 
+
+        queue_push(&queue, node->child_upper_left);
+        queue_push(&queue, node->child_upper_right);
+        queue_push(&queue, node->child_lower_right);
+        queue_push(&queue, node->child_lower_left);
+    }
+
+
+    int num_leaves = 0;
+    int max_size = 0;
+    queue_push(&queue, root);
+
+    while (!is_empty(queue)) {
+        QuadTree *node = queue_pop(queue);
+
+        if (is_leaf(node)) {
+            num_leaves++;
+            max_size = MAX(max_size, node->size);
+            continue;
+        }
+
+        queue_push(&queue, node->child_upper_left);
+        queue_push(&queue, node->child_upper_right);
+        queue_push(&queue, node->child_lower_right);
+        queue_push(&queue, node->child_lower_left);
+    }
+
+    fprintf(fout, "%d\n", get_tree_height(root) + 1);
+    fprintf(fout, "%d\n", num_leaves);
+    fprintf(fout, "%d\n", max_size);
 }
 
 
@@ -37,7 +154,7 @@ void check_command_line_arguments(int argc, char *argv[])
         if (!strcmp(argv[1], "-d") && strlen(argv[1]) == strlen("-d"))
             return;
         
-        fprintf(stderr, "[INFO] Program called with 4 arguments\n");
+        fprintf(stderr, "[INFO] Program called with 3 arguments\n");
         fprintf(stderr, "[ERR]  Expected flag '-d'\n");
         exit(EXIT_FAILURE);
     }
@@ -48,7 +165,7 @@ void check_command_line_arguments(int argc, char *argv[])
         if (!strcmp(argv[1], "-c2") && strlen(argv[1]) == strlen("-c2"))
             return;
         
-        fprintf(stderr, "[INFO] Program called with 5 arguments\n");
+        fprintf(stderr, "[INFO] Program called with 4 arguments\n");
         fprintf(stderr, "[ERR]  Expected either flag '-c1' or '-c2'\n");
         exit(EXIT_FAILURE);
     }
@@ -59,22 +176,22 @@ int main(int argc, char *argv[])
 {
     check_command_line_arguments(argc, argv);
 
-    FILE *fin = fopen(argv[argc - 2], "rb");
-    FILE *fout = fopen(argv[argc - 1], "wt");
+    FILE *fin = fopen(argv[argc - 2], "rb");    // Read binary
+    FILE *fout = fopen(argv[argc - 1], "wt");   // Write text
 
     if (!fin) {
         fprintf(stderr, "[ERR] Failed to open '%s' file\n", argv[argc - 2]);
         exit(EXIT_FAILURE);
     }
 
-    if (strcmp(argv[1], "-c1")) {
+    if (!strcmp(argv[1], "-c1")) {
         int factor = atoi(argv[2]);
         solve_task1(factor, fin, fout);
     }
-    else if (strcmp(argv[1], "-c2")) {
+    else if (!strcmp(argv[1], "-c2")) {
         int factor = atoi(argv[2]);
         solve_task2(factor, fin, fout);
-    } else if (strcmp(argv[1], "-d"))
+    } else if (!strcmp(argv[1], "-d"))
         solve_task3(fin, fout);
 
     return 0;
---
+++ src/utils.h
@@ -0,0 +1,144 @@
+#ifndef _DATA_STRUCTS_H_
+#define _DATA_STRUCTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#define MAX(x, y) ((x > y) ? x : y)
+
+
+typedef struct {
+    unsigned char R;
+    unsigned char G;
+    unsigned char B;
+} color;
+
+
+typedef struct {
+    unsigned int width;
+    unsigned int height;
+    unsigned int max_rgb;
+    color **grid;
+} RawImage;
+
+
+typedef struct TreeNode {
+    unsigned int x, y, size;
+    color pixel;
+    struct TreeNode *child_upper_left;     // Child 1
+    struct TreeNode *child_upper_right;    // Child 2
+    struct TreeNode *child_lower_right;    // Chlid 3
+    struct TreeNode *child_lower_left;     // Child 4
+} QuadTree;
+
+
+
+typedef struct ListNode {
+    QuadTree *root;
+    struct ListNode *next;
+} List;
+
+typedef struct {
+    List *head;
+    List *tail;
+} Queue;
+
+
+QuadTree *new_tree_node(unsigned int x, unsigned  y, unsigned int size)
+{
+    QuadTree *new_node = (QuadTree *) calloc (1, sizeof(QuadTree));
+    new_node->x = x;
+    new_node->y = y;
+    new_node->size = size;
+    new_node->child_lower_left = NULL;
+    new_node->child_lower_right = NULL;
+    new_node->child_upper_left = NULL;
+    new_node->child_upper_right = NULL;
+    return new_node;
+}
+
+List *new_list_node(QuadTree *root)
+{
+    List *new_node = (List *) malloc (sizeof(List));
+    new_node->root = root;
+    new_node->next = NULL;
+    return new_node;
+}
+
+
+int is_leaf(QuadTree *root)
+{
+    return root
+        && !root->child_upper_left
+        && !root->child_upper_right
+        && !root->child_lower_right
+        && !root->child_lower_left;
+}
+
+
+int get_tree_height(QuadTree *root)
+{
+    if (!root)
+        return 0;
+    
+    int height_child1 = get_tree_height(root->child_upper_left);
+    int height_child2 = get_tree_height(root->child_upper_right);
+    int height_child3 = get_tree_height(root->child_lower_right);
+    int height_child4 = get_tree_height(root->child_lower_left);
+
+    int max_height = 0;
+    max_height = MAX(max_height, height_child1);
+    max_height = MAX(max_height, height_child2);
+    max_height = MAX(max_height, height_child3);
+    max_height = MAX(max_height, height_child4);
+    return (max_height + 1);
+}
+
+Queue *new_empty_queue()
+{
+    return (Queue *) malloc (sizeof(Queue));
+}
+
+int is_empty(Queue *queue)
+{
+    return (!queue || !queue->head);
+}
+
+
+QuadTree *queue_pop(Queue *queue)
+{
+    if (is_empty(queue)) {
+        fprintf(stderr, "[ERR] Empty queue\n");
+        return NULL;
+    }
+
+    QuadTree *root = queue->head->root;
+    List *tmp = queue->head;
+    queue->head = queue->head->next;
+    free(tmp);
+    return root; 
+}
+
+void queue_push(Queue **queue, QuadTree *root)
+{
+    if (!(*queue)) {
+        // Queue does not exist
+        *queue = new_empty_queue();
+    }
+
+    if (!(*queue)->head) {
+        // Empty queue
+        (*queue)->head = (*queue)->tail = new_list_node(root);
+        return;
+    }
+
+    // Non-empty queue
+    (*queue)->tail = new_list_node(root);
+    (*queue)->tail = (*queue)->tail->next;
+}
+
+
+
+
+#endif
